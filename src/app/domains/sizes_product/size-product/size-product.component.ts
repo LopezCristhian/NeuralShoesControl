ec5e180cf7a55ff60ec96f951f529ff35fafa021
LopezCristhian <cristhianlopez@udenar.edu.co>
2025-04-29 21:48:56 -0500

    Inicialización proyecto

---
+++ src/app/domains/sizes_product/size-product/size-product.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-size-product',
+  imports: [],
+  templateUrl: './size-product.component.html',
+  styleUrl: './size-product.component.scss'
+})
+export class SizeProductComponent {
+
+}
