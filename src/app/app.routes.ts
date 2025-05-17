1a82ac66bab169d55f457a6da4f90ef91d6e3b37
ZKLUIS999 <lassoluis03@gmail.com>
2025-05-17 17:52:37 -0500

    admin demo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { IndexComponent } from './domains/index/index/index.component';
 import { ProductComponent } from './domains/products/product/product.component';
+import { AdminDemoComponent } from './admin-demo/admin-demo.component';
 
 export const routes: Routes = [
   { path: '', component: IndexComponent },
-  { path: 'productos', component: ProductComponent }
+  { path: 'productos', component: ProductComponent },
+  { path: 'admin-demo', component: AdminDemoComponent }
 ];
