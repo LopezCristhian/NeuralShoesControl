5af800b699c835802262b728ca3400cf8db6f080
Andres4104 <andreseste46810@gmail.com>
2025-05-26 22:44:43 -0500

    pie de pagina

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component'; // 👈 Importa tu componente
+import { PieComponent  } from './pie/pie.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, PieComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
